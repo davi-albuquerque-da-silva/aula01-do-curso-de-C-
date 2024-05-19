c3708a6446a0cb49df4912c01f197fe3607be4a3
davi-albuquerque-da-silva <125098634+davi-albuquerque-da-silva@users.noreply.github.com>
2024-05-18 21:05:09 -0300

    Adicionar arquivos de projeto.

---
+++ AloMundo/Program.cs
@@ -0,0 +1,19 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AloMundo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ola davi");
+            Console.WriteLine("bem vindo ao curso de programaçao c#");
+            Console.ReadLine();
+        }
+    }
+}
